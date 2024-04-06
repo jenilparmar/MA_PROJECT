f8d0014e442a254284a83cf9b558b174fd9c4440
RACHITALAAD <rachitalaad505@gmail.com>
2024-04-06 23:10:26 +0530

    .

--- Mathematicsproject.py
+++ Mathematicsproject.py
@@ -19,7 +19,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = LinearRegression()
 
 # Train the model
-model.fit(X_train, y_train)
+model.fit(X_train, y_train) 
 
 # Make predictions
 y_pred = model.predict(X_test)
