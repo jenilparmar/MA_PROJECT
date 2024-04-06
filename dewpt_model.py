ad301eee6d62566bd6c1c4e100403348fb670bf6
jenilparmar <jerryhash231@gmail.com>
2024-04-06 22:59:34 +0530

    Function maded

--- dewpt_model.py
+++ dewpt_model.py
@@ -5,39 +5,39 @@ from sklearn.metrics import r2_score
 
 # Step 1: Data Preparation
 # Load dataset
-data = pd.read_csv("data\JaipurFinalCleanData.csv")
+def dewpt(data):
+    data = pd.read_csv("data\JaipurFinalCleanData.csv")
 
-# Handling missing values if any
-data.dropna(inplace=True)
+    # Handling missing values if any
+    data.dropna(inplace=True)
 
-# Splitting data into features and target variable
-X = data[['meantempm_1', 'minhumidity_2', 'meanpressurem_1']]
-print(X)
-y = data['meandewptm_2']
-print(y)
+    # Splitting data into features and target variable
+    X = data[['meantempm_1', 'minhumidity_2', 'meanpressurem_1']]
+    # print(X)
+    y = data['meandewptm_2']
+    # print(y)
 
-# Step 2: Model Training
-# Splitting data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7, random_state=42)
-print(X_test,X_train)
+    # Step 2: Model Training
+    # Splitting data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7, random_state=42)
+    # print(X_test,X_train)
 
-# Initialize the linear regression model
-model = LinearRegression()
-#feed the training data
-model.fit(X_train, y_train)
+    # Initialize the linear regression model
+    model = LinearRegression()
+    #feed the training data
+    model.fit(X_train, y_train)
 
-# Step 3: Model Evaluation
-# Predicting on the testing set
-y_pred = model.predict(X_test)
+    # Step 3: Model Evaluation
+    # Predicting on the testing set
+    y_pred = model.predict(X_test)
 
-# Evaluating model performance
-accuracy = r2_score(y_test, y_pred)
-print("Accuracy:", accuracy)
-print("Regression Coefficients:", model.coef_)
-# Step 4: Prediction
-# Assuming new_data contains the features (Temperature, Humidity, Pressure, AirSpeed, RainToday) for a new day
-new_data = pd.DataFrame([[35,6,1006]], columns=['meantempm_1', 'minhumidity_2', 'meanpressurem_1'])
-
+    # Evaluating model performance
+    accuracy = r2_score(y_test, y_pred)
+    # print("Accuracy:", accuracy)
+    # print("Regression Coefficients:", model.coef_)
+    # Step 4: Prediction
+    # Assuming new_data contains the features (Temperature, Humidity, Pressure, AirSpeed, RainToday) for a new day
+    new_data = pd.DataFrame([data], columns=['meantempm_1', 'minhumidity_2', 'meanpressurem_1'])
+    prediction = model.predict(new_data)
 # Making prediction for the new day
-prediction = model.predict(new_data)
-print("Predicted Rainfall Tomorrow:", prediction)
+    return prediction
