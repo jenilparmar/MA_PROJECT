b2b909d29a704917cfa43e2a43fb37d99a1d0cef
MEET2231 <modimeet05@gmail.com>
2024-04-09 11:04:06 +0530

    made a flask app for making model interactive

--- model.py
+++ model.py
@@ -1,19 +1,18 @@
-import pandas as pd # type: ignore
-from sklearn.model_selection import train_test_split # type: ignore
-from sklearn.linear_model import LinearRegression,LogisticRegression # type: ignore
-from sklearn.metrics import r2_score,accuracy_score # type: ignore
-from sklearn.metrics import mean_squared_error, r2_score # type: ignore
-
-# Step 1: Data Preparation
-# Load dataset
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression,LogisticRegression
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error, r2_score
+from flask import Flask ,render_template,request
+app = Flask(__name__)
 def dewpt(dataOP):
     data = pd.read_csv("data\JaipurFinalCleanData.csv")
 
     # Handling missing values if any
     data.dropna(inplace=True)
 
-    # Splitting data into features and target variable
-    X = data[['maxtempm', 'mintempm', 'maxhumidity_1', 'minhumidity_1','maxdewptm_1','mindewptm_1','maxpressurem_1','minpressurem_1','precipm_1']]
+   # Splitting data into features and target variable
+    X = data[['maxtempm', 'mintempm', 'maxhumidity_1', 'minhumidity_1','maxdewptm_1','mindewptm_1','maxpressurem_1','minpressurem_1']]
     y = data['meandewptm_2']
 
     # Step 2: Model Training
@@ -34,118 +33,70 @@ def dewpt(dataOP):
 
     # Step 4: Prediction
     # Assuming new_data contains the features (Temperature, Humidity, Pressure, AirSpeed, RainToday) for a new day
-    new_data = pd.DataFrame([dataOP], columns=['maxtempm', 'mintempm', 'maxhumidity_1', 'minhumidity_1','maxdewptm_1','mindewptm_1','maxpressurem_1','minpressurem_1','precipm_1'])
+    new_data = pd.DataFrame([dataOP], columns=['maxtempm', 'mintempm', 'maxhumidity_1', 'minhumidity_1','maxdewptm_1','mindewptm_1','maxpressurem_1','minpressurem_1'])
     prediction = model.predict(new_data)
     return prediction
 def temp(dataOP):
     weather_data = pd.read_csv("data\JaipurFinalCleanData.csv")
 
-    # Select features (X) and target variable (y)
-    X = weather_data[['maxtempm', 'mintempm', 'maxhumidity_1', 'minhumidity_1','maxdewptm_1','mindewptm_1','maxpressurem_1','minpressurem_1','precipm_1']]  
+    X = weather_data[['maxtempm', 'mintempm', 'maxhumidity_1', 'minhumidity_1','maxdewptm_1','mindewptm_1','maxpressurem_1','minpressurem_1']]  
     y = weather_data['meantempm']  
 
-    # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Initialize linear regression model
     model = LinearRegression()
 
-    # Train the model
     model.fit(X_train, y_train)
 
-    # Make predictions
     y_pred = model.predict(X_test)
 
-    # Calculate mean squared error
     mse = mean_squared_error(y_test, y_pred)
-    # print("Mean Squared Error:", mse)
-
-    # Calculate R-squared
+  
     r_squared = r2_score(y_test, y_pred)
-    # print("R-squared:", r_squared)
-
-    # Plot actual vs predicted values
-    # plt.scatter(y_test, y_pred)
-    # plt.xlabel("Actual Mean_Tempareture (mm)")
-    # plt.ylabel("Predicted Mean_Tempareture (mm)")
-    # plt.title("Actual vs Predicted Tempareture")
-    # plt.show()
-    new_data = pd.DataFrame([dataOP], columns=['maxtempm', 'mintempm', 'maxhumidity_1', 'minhumidity_1','maxdewptm_1','mindewptm_1','maxpressurem_1','minpressurem_1','precipm_1'])
+    
+    new_data = pd.DataFrame([dataOP], columns=['maxtempm', 'mintempm', 'maxhumidity_1', 'minhumidity_1','maxdewptm_1','mindewptm_1','maxpressurem_1','minpressurem_1'])
 
     # Making prediction for the new day
     prediction = model.predict(new_data)
     # print("Predicted Mean Tomorrow's Temperature:", prediction)
     return prediction
-# print(dewpt([41,27,5,12,12,-2,1009,1000,0]))
-
-
-
-def humidity(dataOP):
-    weather_data = pd.read_csv('data\\JaipurFinalCleanData.csv')
-
-# Select features (X) and target variable (y)
-    X = weather_data[['maxtempm', 'mintempm', 'maxhumidity_1','minhumidity_1','maxdewptm_1','mindewptm_1','maxpressurem_1','minpressurem_1','precipm_1']]  
-    y = weather_data['maxhumidity_2']  
-
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # Initialize linear regression model
-    model = LinearRegression()
-
-    # Train the model
-    model.fit(X_train, y_train) 
-
-    # Make predictions
-    y_pred = model.predict(X_test)
-    print("Maximum_Humidity : ",y_pred)
-
-    # Calculate mean squared error
-    mse = mean_squared_error(y_test, y_pred)
-    print("Mean Squared Error:", mse)
-
-    # Calculate R-squared
-    r_squared = r2_score(y_test, y_pred)
-    print("R-squared:", r_squared)
-
-    # Plot actual vs predicted values
-    # plt.scatter(y_test, y_pred)
-    # plt.xlabel("Actual Maximum_Humidity (g/m^3)")
-    # plt.ylabel("Predicted Maximum_Humidity (g/m^3)")
-    # plt.title("Actual vs Predicted Humidity")
-    # plt.show()
-    new_data = pd.DataFrame([dataOP], columns=['maxtempm', 'mintempm', 'maxhumidity_1','minhumidity_1','maxdewptm_1','mindewptm_1','maxpressurem_1','minpressurem_1','precipm_1'])
-
-    # Making prediction for the new day
-    prediction = model.predict(new_data)
-    return prediction
 def rainfall(data):
     weather_data = pd.read_csv("data\JaipurFinalCleanData _NextDay.csv")
-    X = weather_data[['maxtempm', 'mintempm', 'maxhumidity_1', 'minhumidity_1','maxdewptm_1','mindewptm_1','maxpressurem_1','minpressurem_1','precipm_2_B']]  
+    X = weather_data[['maxtempm', 'mintempm', 'maxhumidity_1', 'minhumidity_1','maxdewptm_1','mindewptm_1','maxpressurem_1','minpressurem_1']]  
     y = weather_data['precipm_2_B_nextday']  
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     model = LogisticRegression()
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
     # print("Precipitation on test data : ",y_pred)
-    accuracy = accuracy_score(y_test, y_pred)
-    print(f"Accuracy: {accuracy}")
-    new_data = pd.DataFrame([data], columns=['maxtempm', 'mintempm', 'maxhumidity_1','minhumidity_1','maxdewptm_1','mindewptm_1','maxpressurem_1','minpressurem_1','precipm_2_B'])
+
+    new_data = pd.DataFrame([data], columns=['maxtempm', 'mintempm', 'maxhumidity_1','minhumidity_1','maxdewptm_1','mindewptm_1','maxpressurem_1','minpressurem_1'])
 
     # Making prediction for the new day
     prediction = model.predict(new_data)
     return prediction
 
-
-
-    pass
-
-data = [41,27,5,12,12,-2,1009,1000,0]
-K=temp(data)
-print(K)
-D=dewpt(data)
-print(D)
-H=humidity(data)
-print(H)
-P = rainfall(data)
-print(P)
+@app.route('/', methods=["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        # Get the input values from the form
+        maxtempm = float(request.form['maxtempm'])
+        mintempm = float(request.form['mintempm'])
+        maxhumidity_1 = float(request.form['maxhumidity_1'])
+        minhumidity_1 = float(request.form['minhumidity_1'])
+        maxdewptm_1 = float(request.form['maxdewptm_1'])
+        mindewptm_1 = float(request.form['mindewptm_1'])
+        maxpressurem_1 = float(request.form['maxpressurem_1'])
+        minpressurem_1 = float(request.form['minpressurem_1'])
+        
+        # Call the function to make predictions using these input values
+        dewpt_prediction = dewpt([maxtempm, mintempm, maxhumidity_1, minhumidity_1, maxdewptm_1, mindewptm_1, maxpressurem_1, minpressurem_1])
+        temp_prediction = temp([maxtempm, mintempm, maxhumidity_1, minhumidity_1, maxdewptm_1, mindewptm_1, maxpressurem_1, minpressurem_1])
+        r_fall = rainfall([maxtempm, mintempm, maxhumidity_1, minhumidity_1, maxdewptm_1, mindewptm_1, maxpressurem_1, minpressurem_1])
+        # Render the template with the predictions
+        print(r_fall)
+        return render_template("predict.html", dewpt_prediction=round(dewpt_prediction[0],2), temp_prediction=round(temp_prediction[0],2),rainfall=r_fall[0])
+    else:
+        return render_template("index.html")
+if __name__ == '__main__':
+    app.run(debug=True)
